4e76cf9caf2716c8733f7de99ad3068d0dc55f97
sidecus <sidecus@users.noreply.github.com>
2020-06-23 19:26:11 +0000

    deploy: 7e8987ac79db857ea42ce47cdc49083ad6050dc9

---
+++ precache-manifest.60b510bcdd1a5e2f3a1a97492fd34c1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468547eef99ac8129b2bbd27567bd59b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e7653748df29c1edd64",
+    "url": "./static/js/2.7f006e18.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.7f006e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3550401db10b7eb9f0cf",
+    "url": "./static/js/main.35ef7f80.chunk.js"
+  },
+  {
+    "revision": "edfd06ad7940add76953",
+    "url": "./static/js/runtime-main.f8e8b893.js"
+  }
+]);
